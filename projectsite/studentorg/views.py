3e84087aa8791ee247345df89add2c54aa29a096
Kenzo-gg <k3nzkie122@gmail.com>
2026-09-01 23:13:49 +0800

    added dashboard

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,20 +3,53 @@ from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.utils import timezone
+
 
 from studentorg.models import College, Organization, Program, Student, OrgMember
 from studentorg.forms import OrganizationForm, ProgramForm, StudentForm, CollegeForm, OrgMemberForm
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
     ordering = ["college__college_name","name"]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return qs
 
 class OrganizationCreateView(CreateView):
     model = Organization
